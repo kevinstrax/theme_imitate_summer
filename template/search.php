bb4b59ecf800e8eb2257c0ade54f39e18e4a47b7
yuxian <yuxian@pinduoduo.com>
2021-08-22 00:42:25 +0800

    seach page

--- template/search.php
+++ template/search.php
@@ -10,28 +10,33 @@
   <div class="container" id="divAll">
     <div id="divPage">
       <div id="divMiddle">
-        <div id="divTop" class="row header">
-          <h1 id="BlogTitle" class="col-sm header-logo"><a href="{$host}">{$name}</a></h1>
-        </div>
-        <div id="divNavBar" class="row">
-          <ul class="col-md nav">
-            {module:navbar}
-          </ul>
-        </div>
-        <div id="divMain">
-          <div class="post istop istop-post">
-            <h2 class="post-title">{$article.Title}</h2>
+        <div class="header-wrap">
+          <div id="divTop" class="row header">
+            <h1 id="BlogTitle" class="col-sm header-logo"><a href="{$host}">{$name}</a></h1>
           </div>
-          {foreach $articles as $article}
-          {template:post-search}
-          {/foreach}
-          {if count($articles)>0}
-          <div class="pagebar">{template:pagebar}</div>
-          {/if}
+          <div id="divNavBar" class="row">
+            <ul class="col-md nav">
+              {module:navbar}
+            </ul>
+          </div>
         </div>
-        <div id="divSidebar">
-          {template:sidebar}
+        <div class="row main-content">
+          <div id="divMain" class="col-lg-9 order-lg-2">
+            <div class="post istop istop-post">
+              <h2 class="post-title">{$article.Title}</h2>
+            </div>
+            {foreach $articles as $article}
+            {template:post-search}
+            {/foreach}
+            {if count($articles)>0}
+            <div class="pagebar">{template:pagebar}</div>
+            {/if}
+          </div>
+          <div id="divSidebar" class="col-lg-3 order-lg-1">
+            {template:sidebar}
+          </div>
         </div>
+
         {template:footer}
       </div>
       <div class="clear"></div>
