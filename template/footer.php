c9f6f98d7cd4b8e20eab21b05bb44091289bf4f4
yuxian <yuxian@pinduoduo.com>
2021-08-21 16:15:37 +0800

    导航栏样式

--- template/footer.php
+++ template/footer.php
@@ -1,7 +1,7 @@
 {* Template Name:公共底部 *}
-<div id="divBottom">
-  <h4 id="BlogPowerBy">Powered By {$zblogphphtml}</h4>
-  <h3 id="BlogCopyRight">{$copyright}</h3>
+<div id="divBottom" class="row footer">
+  <h4 id="BlogPowerBy" class="footer-copyright">Powered By {$zblogphphtml}</h4>
+  <h3 id="BlogCopyRight" class="footer-copyright">{$copyright}</h3>
 </div>
 <div class="clear"></div>
 
