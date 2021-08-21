ad0647edc3e28fd8a642f62450ec8a939ac5dc4a
yuxian <yuxian@pinduoduo.com>
2021-08-21 16:22:02 +0800

    footer

--- template/footer.php
+++ template/footer.php
@@ -1,7 +1,7 @@
 {* Template Name:公共底部 *}
 <div id="divBottom" class="row footer">
-  <h4 id="BlogPowerBy" class="footer-copyright">Powered By {$zblogphphtml}</h4>
-  <h3 id="BlogCopyRight" class="footer-copyright">{$copyright}</h3>
+  <p id="BlogPowerBy" class="footer-copyright">Powered By {$zblogphphtml}</p>
+  <p id="BlogCopyRight" class="footer-copyright">{$copyright}</p>
 </div>
 <div class="clear"></div>
 
