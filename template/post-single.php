d7c49626101b707360cd250b2a54a3c3a7acf2e6
yuxian <yuxian@pinduoduo.com>
2021-08-22 02:13:56 +0800

    art page

--- template/post-single.php
+++ template/post-single.php
@@ -1,13 +1,14 @@
 {* Template Name:文章页文章内容 *}
 <div class="post single">
-	<h2 class="post-title">{$article.Title}<span class="post-date">{$article.Time()}</span></h2>
+	<h2 class="post-title">{$article.Title}</h2>
 	<div class="post-body">{$article.Content}</div>
+	<h6 class="post-footer">
+		<span class="post-date">{$article.Time()}</span>
+		{$lang['msg']['author']}:{$article.Author.StaticName} <small>|</small> {$lang['msg']['category']}:{$article.Category.Name} <small>|</small> {$lang['default']['view']}:{$article.ViewNums} <small>|</small> {$lang['msg']['comment']}:{$article.CommNums}
+	</h6>
 	<h5 class="post-tags">
                 {if count($article.Tags)>0}{$lang['msg']['tags']}:{foreach $article.Tags as $i => $tag}&nbsp;<a href='{$tag.Url}' title='{$tag.Name}'>{$tag.Name}</a>&nbsp;{if count($article.Tags) > $i}<small>,</small>{/if}{/foreach}{/if}
 	</h5>
-	<h6 class="post-footer">
-		{$lang['msg']['author']}:{$article.Author.StaticName} <small>|</small> {$lang['msg']['category']}:{$article.Category.Name} <small>|</small> {$lang['default']['view']}:{$article.ViewNums} <small>|</small> {$lang['msg']['comment']}:{$article.CommNums}
-	</h6>
 </div>
 
 {if !$article.IsLock}
