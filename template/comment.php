79a24547e72368d753579c0d35d0ddf263c55af9
yuxian <yuxian@pinduoduo.com>
2021-08-24 02:30:38 +0800

    add avatar

--- template/comment.php
+++ template/comment.php
@@ -1,10 +1,11 @@
 {* Template Name:单条评论 *}
 <ul class="msg" id="cmt{$comment.ID}">
 	<li class="msgname">
-		<small>&nbsp;{$lang['default']['comment_post_on']}&nbsp;{$comment.Time()}&nbsp;&nbsp;</small>
+		<img class="avatar" src="{$comment.Author.Avatar}" alt="" width="32"/>
 		<span class="commentname">
 			<a href="{$comment.Author.HomePage}" rel="nofollow" target="_blank">{$comment.Author.StaticName}</a>
 		</span>
+		<small>&nbsp;{$lang['default']['comment_post_on']}&nbsp;{$comment.Time()}&nbsp;&nbsp;</small>
 		<span class="revertcomment">
 			<a href="#comment" onclick="zbp.comment.reply('{$comment.ID}')">回复</a>
 		</span>
