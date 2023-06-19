1059a0e569e2ced95328a14e80f08854655a1167
kevinstrax <847967921@qq.com>
2023-06-20 01:31:55 +0800

    modify article list styles

--- template/post-multi.php
+++ template/post-multi.php
@@ -1,7 +1,11 @@
 {* Template Name:列表页普通文章 *}
 <section class="post multi">
 	<h2 class="post-title"><a href="{$article.Url}">{$article.Title}</a></h2>
-	<div class="post-body">{$article.Intro}</div>
+	<div class="post-body">
+		<p class="post-body-intro">{$article.Intro}</p>
+		<div class="post-body-intromask"></div>
+	</div>
+	<a class="more-link float-end" href="{$article.Url}" alt="{$article.Title}">阅读全文>></a>
 	<h6 class="post-footer">
 		{$lang['msg']['author']}:{$article.Author.StaticName} <small>|</small> {$lang['msg']['category']}:{$article.Category.Name} <small>|</small> 阅读:{$article.ViewNums} <small>|</small> {$lang['msg']['comment']}:{$article.CommNums}
 		<span class="post-date">{$article.Time()}</span>
