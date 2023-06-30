fccf1a5ebc05a3223f41d4de1d185a329fbcd148
kevinstrax <847967921@qq.com>
2023-07-01 04:05:12 +0800

    update index.php

---
+++ include/searchpanel.php
@@ -0,0 +1,6 @@
+<div>
+    <form name="search" method="post" action="https://www.aquac.cc/zb_system/cmd.php?act=search" class="row g-2">
+        <input type="text" name="q" size="11" class="form-control col-auto rounded-0 rounded-start border-end-0"  />
+        <input type="submit" value="搜索" class="btn btn-outline-secondary col-auto rounded-0 rounded-end " />
+    </form>
+</div>
--- template/index.php
+++ template/index.php
@@ -7,21 +7,31 @@
 </head>
 
 <body class="multi {$type}">
+    <nav class="navbar navbar-expand-lg brand-bar">
+        <div class="container-fluid">
+          <a class="navbar-brand text-uppercase font-monospace" href="{$host}">Aquac.cc</a>
+        </div>
+      </nav>
+      
+      <div class="header-wrap container-fluid mb-5">
+        <div class="header-wrap-box row">
+            <header id="divTop" class="header">
+                <h1 id="BlogTitle" class="col-sm header-logo text-center mb-5 fw-bold">
+                    <a href="${host}">{$name}<i id="BlogSubTitle" alt="{$subname}"
+                            style="background-image: url(https://image.aquac.cc/website/aquac/2023/06/202306125294_7344.png);"></i></a>
+                </h1>
+            </header>
+            <nav id="divNavBar" class="row">
+                <ul class="col-md nav justify-content-center fw-bold">
+                  {module:navbar}
+                </ul>
+            </nav>
+        </div>
+    </div>
+
   <div class="container" id="divAll">
     <div id="divPage">
       <div id="divMiddle">
-        <div class="header-wrap">
-          <header id="divTop" class="row header">
-            <h1 id="BlogTitle" class="col-sm header-logo">
-                <a href="{$host}">{$name}<img id="BlogSubTitle" alt="{$subname}" src="https://image.aquac.cc/website/aquac/2023/06/202306125294_7344.png"/></a>
-	        </h1>
-          </header>
-          <nav id="divNavBar" class="row">
-            <ul class="col-md nav">
-              {module:navbar}
-            </ul>
-          </nav>
-        </div>
         <div class="row main-content">
           <div id="divMain" class="col-lg-9 order-lg-2">
             {foreach $articles as $article}
